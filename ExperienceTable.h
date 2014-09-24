5d43d9a9b916810f2b877c5c85cd48a5ce54bc00
hahajoker181 <akiraff50@gmail.com>
2014-09-24 14:17:21 +1000

    Create ExperienceTable.h

---
+++ ExperienceTable.h
@@ -0,0 +1,16 @@
+#ifndef EXPERIENCETABLE_H
+#define EXPERIENCETABLE_H
+
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+class ExperienceTable
+{
+    public:
+        void showTrueExp();
+
+};
+
+#endif // EXPERIENCETABLE_H
