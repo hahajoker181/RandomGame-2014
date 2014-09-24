74e20a63a9bc01ac0c992bb85777139180074b79
hahajoker181 <akiraff50@gmail.com>
2014-09-24 14:48:10 +1000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Hahajoker181 (Chan 06)
+Date: 20 September 2014
+Purpose: Replicate a game similar to RuneScape.
+*/
+#include <iostream>
+
+#include "LoginUser.h"
+
+using namespace std;
+
+int main()
+{
+    //Debugging LoginUser
+    LoginUser lu;
+    lu.display();
+}
