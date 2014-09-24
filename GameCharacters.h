fd9b82033b2dbe375dace755e1a65d84347dd8d9
hahajoker181 <akiraff50@gmail.com>
2014-09-24 14:23:24 +1000

    Create GameCharacters.h

---
+++ GameCharacters.h
@@ -0,0 +1,21 @@
+#ifndef GAMECHARACTERS_H
+#define GAMECHARACTERS_H
+
+#include <iostream>
+
+#include "UpgradeTiers.h"
+#include "MessageSystem.h"
+
+using namespace std;
+
+class GameCharacters
+{
+    public:
+        void NPC_rondolf();
+        void NPC_xavier();
+        void NPC_robinhood();
+    protected:
+    private:
+};
+
+#endif // GAMECHARACTERS_H
