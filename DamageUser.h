ad40a21f7beaa096be46783414bd425d26a6ed49
hahajoker181 <akiraff50@gmail.com>
2014-09-24 14:15:49 +1000

    Create DamageUser.h

---
+++ DamageUser.h
@@ -0,0 +1,16 @@
+#ifndef DAMAGEUSER_H
+#define DAMAGEUSER_H
+
+#include <iostream>
+
+using namespace std;
+
+class DamageUser
+{
+    public:
+        void testDamage();
+    protected:
+    private:
+};
+
+#endif // DAMAGEUSER_H
